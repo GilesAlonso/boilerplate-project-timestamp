e90f0e13ff00419803b58e89192139df4e078233
Giles <86449189+GilesAlonso@users.noreply.github.com>
2025-01-13 09:57:18 -0300

    Update index.js added mongoDB routes for url shortening

--- index.js
+++ index.js
@@ -4,20 +4,102 @@
 // init project
 var express = require('express');
 var app = express();
+const validUrl = require('valid-url');
+const shortid = require('shortid');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// Middleware
+app.use(express.json());
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+
+// MongoDB Connection
+const mongoUri = process.env.MONGO_URI; // Mongo URI from environment variable
+if (!mongoUri) {
+  console.error('MONGO_URI is not defined!');
+  process.exit(1); // Exit if MongoDB URI is not provided
+}
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+  });
+
+// MongoDB Schema for URL shortener
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: String
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
+// URL Shortener Routes
+app.post('/api/shorturl', (req, res) => {
+  const { original_url } = req.body;
+
+  // Check if the URL is valid
+  if (!validUrl.isWebUri(original_url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  // Generate a unique short URL
+  const shortUrl = shortid.generate();
+
+  // Save to MongoDB
+  const newUrl = new Url({
+    original_url,
+    short_url: shortUrl
+  });
+
+  newUrl.save()
+    .then(() => {
+      res.json({
+        original_url,
+        short_url: shortUrl
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Server Error');
+    });
+});
+
+app.get('/api/shorturl/:shorturl', (req, res) => {
+  const { shorturl } = req.params;
+
+  // Find the corresponding URL in the database
+  Url.findOne({ short_url: shorturl })
+    .then(url => {
+      if (!url) {
+        return res.status(404).json({ error: 'No URL found for this short URL' });
+      }
+
+      // Redirect to the original URL
+      res.redirect(url.original_url);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send('Server Error');
+    });
+});
 
 // /api/whoami endpoint
 app.get("/api/whoami", (req, res) => {
